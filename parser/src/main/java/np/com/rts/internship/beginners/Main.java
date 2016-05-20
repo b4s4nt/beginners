b5af8d8de049b6d7ee61fe2d671ffa719ff5688d
basanta kandel <basantakandel10@gmail.com>
2016-05-20 20:59:44 +0545

    advanced parser is done here

--- parser/src/main/java/np/com/rts/internship/beginners/Main.java
+++ parser/src/main/java/np/com/rts/internship/beginners/Main.java
@@ -1,5 +1,6 @@
 package np.com.rts.internship.beginners;
 
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 
@@ -13,10 +14,83 @@ public class Main {
   double value;
 
   public Main() {
+    char a=0;
+    char b=0;
+    char c=0;
+    String s=" ";
+
+
+    try {
+
+      byte[] bytes = parseThis.getBytes("US-ASCII");
+      for(int i=0;i<parseThis.length();i++){
+        // System.out.print(bytes[i]-1);
+        char e=  (char) (bytes[i]-1);
+        s=  s+e;// this is concatination of each character corrresponds to each bytes or ascii
+        //System.out.println(s);
+        //s.replace("null", "");
+        //System.out.println(s);
+
+      }
+
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+
+
+    // System.out.println(s);
+
     // Write your code here
+    for(int i=0;i< s.length();i++){
+      char a_char = s.charAt(i);
+
+
+      if(i==21){
+        a=a_char;
+
+      }
+      if(i==22){
+
+        b=a_char;
+
+
+
+      }
+      if(i==23){
+
+        c= a_char;
+
+      }
+
+    }
+
+
+
+
+
+
+
+
+    String s1 = Character.toString(a);
+    String s2 = Character.toString(b);
+    String s3 = Character.toString(c);
+    String s4=s2+s3;// this is give 3.0 as string value
+
+
+
+
+    value =Double.parseDouble(s4); //converting string to double
+
+    System.out.println(value);
+
+
+
+
+
 
 
   }
+
 
   public static void main(String[] args) {
     new Main();
